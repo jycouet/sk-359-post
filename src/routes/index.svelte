8308c1d69d36f6b401c302b6fbd682796c80aa66
jycouet <jycouet@gmail.com>
2022-07-06 15:40:48 +0200

    doing a GET => OK doing a POST => NOK

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
+<script context="module" lang="ts">
+  import type { LoadEvent } from "@sveltejs/kit";
+
+  export async function load(event: LoadEvent) {
+    // Working well
+    const resultGET = await event.fetch("http://localhost:3000/api", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const jsonGET = await resultGET.json();
+    console.log(`jsonGET`, jsonGET);
+
+    // Not working from version 359
+    // Error message: "Request with GET/HEAD method cannot have body."
+    const resultPOST = await event.fetch("http://localhost:3000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        hello: "World",
+      }),
+    });
+    const jsonPOST = await resultPOST.json();
+    console.log(`jsonPOST`, jsonPOST);
+
+    return { props: {} };
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
